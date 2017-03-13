9bd259fbdddb2a30b810638fd26ba13da9be8585
Daniel Rojkind <daniel@dpc.io>
2017-03-14 01:05:39 +1300

    fully functioning clock, need to fix ticking errors when it ticks over at 12 o'clock mark

--- dynamic.js
+++ dynamic.js
@@ -1,12 +1,25 @@
 const secondHand = document.querySelector('.secondHand');
+const minuteHand = document.querySelector('.minuteHand');
+const hourHand = document.querySelector('.hourHand');
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-    secondHand.style.background = `blue`;
-    console.log(seconds * 6);
+
+    const minutes = now.getMinutes();
+    const minuteDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+    const hours = now.getHours();
+
+    const hourDegrees = ((hours / 12) * 360) + 90;
+
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    console.log(hours + " is this working?")
+
+
 }
 
 setInterval(setDate, 1000);
