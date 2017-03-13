87d48cb0e9464fbf86e13a6d4e80aa52c47f8598
Daniel Rojkind <daniel@dpc.io>
2017-03-14 01:13:17 +1300

    Added comments

--- dynamic.js
+++ dynamic.js
@@ -3,23 +3,22 @@ const minuteHand = document.querySelector('.minuteHand');
 const hourHand = document.querySelector('.hourHand');
 
 function setDate() {
+	//Get current date 
     const now = new Date();
+    //Modify seconds
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
+    //Modify minutes
     const minutes = now.getMinutes();
     const minuteDegrees = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
+    //Modify Hour Hand
     const hours = now.getHours();
-
     const hourDegrees = ((hours / 12) * 360) + 90;
-
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    console.log(hours + " is this working?")
-
-
 }
 
 setInterval(setDate, 1000);
